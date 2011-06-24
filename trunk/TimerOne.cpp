926dc74e9216ca384a14298209639b5a423e130c
lex.v.talionis@gmail.com <lex.v.talionis@gmail.com@cddbf931-821a-b966-68ca-05c7c0cb5625>
2011-06-24 03:39:51 +0000

    Changed comments to point to google code group

--- trunk/TimerOne.cpp
+++ trunk/TimerOne.cpp
@@ -3,12 +3,14 @@
  *  Original code by Jesse Tane for http://labs.ideo.com August 2008
  *  Modified March 2009 by Jérôme Despatis and Jesse Tane for ATmega328 support
  *  Modified June 2009 by Michael Polli and Jesse Tane to fix a bug in setPeriod() which caused the timer to stop
+ *  Modified June 2011 by Lex Talionis to add a function to read the timer
  *
  *  This is free software. You can redistribute it and/or modify it under
  *  the terms of Creative Commons Attribution 3.0 United States License. 
  *  To view a copy of this license, visit http://creativecommons.org/licenses/by/3.0/us/ 
  *  or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
  *
+ *  See Google Code project http://code.google.com/p/arduino-timerone/ for latest
  */
 #ifndef TIMERONE_cpp
 #define TIMERONE_cpp
